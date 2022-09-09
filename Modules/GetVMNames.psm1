059395ab0b59d5efdf14e7e08159a7c54472255e
Obediyah McNelly <obiwanmcnoby@gmail.com>
2022-09-08 20:56:17 -0400

    Made style corrections

--- Modules/GetVMNames.psm1
+++ Modules/GetVMNames.psm1
@@ -3,14 +3,11 @@
 
 function GetVMNames {
 	$Serverlist = @()
-	
-	Do 
-	{ 
+	Do { 
 		$vmName = Read-Host -Prompt "Enter the host name of the VM you'd like to pull settings for"
 		if ($vmName -ne '') {$Serverlist += $vmName}
 		$Response = Read-Host -Prompt "Would you like to add additional servers to this list? (y/n)"
 		
-	}
-	Until ($Response -eq 'n')
+	} Until ($Response -eq 'n')
 	return $Serverlist
 }
